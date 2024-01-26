1984b971598dd7a813d53847e1a383a4433fb326
nazarii.kishman <nazarii.kishman@infotech.gov.ua>
2024-01-26 14:23:59 +0200

    07-Generic-Homework

--- index.ts
+++ index.ts
@@ -1,24 +1,74 @@
 
-// Вам потрібно створити умовний тип, що служить для встановлення типу, що повертається з функції. 
-// Як параметр типу повинен обов'язково виступати функціональний тип.
+//Напишіть узагальнену функцію filterArray(array, condition), яка фільтрує масив елементів на основі наданої умови.
 
-type ReturnFunctionType<T> = T extends () => infer returnType ? returnType : undefined;
-
-function testFunction(): string {
-  return 'bla';
+function sortArray<T>(arr: T[], condition: (a: T, b: T) => number): T[] {
+  return arr.slice().sort(condition);;
 }
 
-type TestReturnType = ReturnFunctionType<typeof testFunction>;
+let array = [12, 5, 3, 1, 0];
+let sortedArr = sortArray(array, (a, b) => a - b);
+console.log(sortedArr);
 
+//Створіть узагальнений клас Stack, який являє собою стек елементів з методами push, pop і peek.
 
-// Вам потрібно створити умовний тип, який приймає функціональний тип з одним параметром (або задовільним) 
-// та повертає кортеж, де перше значення - це тип, що функція повертає, а другий - тип її параметру
-
-type ReturnFunctionInfo<T> = T extends (param: infer paramType) => infer returnType ? [returnType, paramType] : undefined;
-
-
-  function testFunc(param: number): string {
-    return 'bla'; 
-  }
+class Stack<T> {
   
-  type TestReturnTypee = ReturnFunctionInfo<typeof testFunc>;
+  private items: T[] = [];
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  peek(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+}
+
+const stack = new Stack<number>();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(5);
+
+console.log(stack.peek());
+
+console.log(stack.pop());
+
+console.log(stack.peek()); 
+
+
+//Створіть узагальнений клас Dictionary, який являє собою словник (асоціативний масив) 
+//з методами set, get і has. Обмежте ключі тільки валідними типами для об'єкта
+
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+class Dictionary<T extends object> {
+  
+ private data: { [key: string]: T } = {}
+
+ set(key: string, value: T): void {
+  this.data[key] = value;
+ }
+
+ get(key: string): T | undefined {
+  return this.data[key];
+ }
+
+ has(key: string): boolean {
+  return key in this.data;
+ }
+
+}
+
+let personDictionary = new Dictionary<IPerson>();
+personDictionary.set('Nazar', { name: 'Nazar', age: 31 });
+
+console.log(personDictionary.has('Nazar')); // true
