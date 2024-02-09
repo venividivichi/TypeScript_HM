2ba28a25406c190484371dab8c16109d08e99514
nazarii.kishman <nazarii.kishman@infotech.gov.ua>
2024-02-09 15:13:18 +0200

    12-Homework

--- index.ts
+++ index.ts
@@ -1,59 +1,109 @@
 
-
-//Вам потрібно написати функцію, яка повертатиме об'єкт, де буде властивість result і це буде паліндром, 
-//і властивість steps — це число викликів до знаходження паліндрома. Для того, щоб перевірити себе 
-//використовуйте число 196. Це так зване Lychrel number — число яке немає поліндрому.
-
-function isPalindrome(num: number): boolean {
-  let strNum = num.toString();
-  return strNum === strNum.split('').reverse().join('');
-}
-
-function generatePalindrome(num: number): { result: number | null; steps: number } {
-  
-  let steps = 0;
-  let currentNumber = num;
-  let maxSteps = 1000;
-  while (!isPalindrome(currentNumber)) {
-      let reversedNumber = parseInt(currentNumber.toString().split('').reverse().join(''), 10);
-      currentNumber += reversedNumber;
-      steps++;
-      if (steps >= maxSteps) {
-          return { result: null, steps };
-      }
-  }
-  return { result: currentNumber, steps };
-}
-
-let result = generatePalindrome(96);
-console.log(result);
-
-
-//Напишіть функцію, яка приймає масив унікальних елементів і генерує всі можливі перестановки цього масиву.
-//Використовуйте рекурсію для знаходження всіх перестановок. Наприклад, якщо вхідний масив [1, 2, 3], 
-//функція має повернути масив, що містить [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [2, 3, 1], [3, 1, 2] і [3, 2, 1].
-
-function genCombination(arr: number[]): number[][] {
-  
-  let result: number[][] = [];
-
-  function combine(start: number): void {
+function DeprecatedMethod(reason: string, alternativeMethod?: string): any {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
       
-    if (start === arr.length - 1) {
-        result.push([...arr]);
-        return;
-    }
-
-    for (let i = start; i < arr.length; i++) {
-        [arr[start], arr[i]] = [arr[i], arr[start]];
-        combine(start + 1);
-        [arr[start], arr[i]] = [arr[i], arr[start]];
-    }
+        let originalMethod = descriptor.value;
+  
+        descriptor.value = function(...args: any[]) {
+            console.warn(`Warning: Method '${String(propertyKey)}' is deprecated. ${reason}`); 
+            if (alternativeMethod) {
+                console.warn(`Please use this ${alternativeMethod}.`);
+            }
+            return originalMethod.apply(this, args);
+        };  
+  
+      return descriptor
+    };
   }
 
-  combine(0);
-  return result;
+  function MinLength(minLength: number): PropertyDecorator {
+    return function(target: any, propertyKey: string | symbol) {
+      let value = target[propertyKey];
+  
+      let getter = () => value;
+      let setter = (newValue: string) => {
+        if (newValue.length < minLength) {
+          throw new Error(`Error: The length of '${String(propertyKey)}' should be at least ${minLength} characters.`);
+        }
+        value = newValue;
+      };
+  
+      Object.defineProperty(target, propertyKey, {
+        get: getter,
+        set: setter,
+        enumerable: true,
+        configurable: true,
+      });
+    };
+  }
+
+  function Email(): PropertyDecorator {
+    return function(target: any, propertyKey: string | symbol) {
+      let value = target[propertyKey];
+  
+      let getter = () => value;
+      let setter = (newValue: string) => {
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!emailRegex.test(newValue)) {
+          throw new Error(`Error: The value of '${String(propertyKey)}' is not a valid email address.`);
+        }
+        value = newValue;
+      };
+  
+      Object.defineProperty(target, propertyKey, {
+        get: getter,
+        set: setter,
+        enumerable: true,
+        configurable: true,
+      });
+    };
+  }
+
+  function MaxLength(maxLength: number): PropertyDecorator {
+    return function(target: any, propertyKey: string | symbol) {
+      let value = target[propertyKey];
+  
+      let getter = () => value;
+      let setter = (newValue: string) => {
+        if (newValue.length > maxLength) {
+          throw new Error(`Error: The length of "${String(propertyKey)}" cannot exceed ${maxLength} characters.`);
+        }
+        value = newValue;
+      };
+  
+      Object.defineProperty(target, propertyKey, {
+        get: getter,
+        set: setter,
+        enumerable: true,
+        configurable: true,
+      });
+    };
+  }
+
+
+class MyClass {
+    
+    @MinLength(3)
+    @MaxLength(15)
+    username: string;
+  
+    @Email()
+    email: string;
+
+    constructor(username: string, email: string) {
+        this.username = username;
+        this.email = email;
+      }
+
+    @DeprecatedMethod('This method is to old.', 'newMethod')
+    public oldMethod(): any {
+        console.log('Old method is called.');
+    }
+
+    public newMethod(): any {
+        console.log('New method is called.');
+    }
 }
 
-let testArray = [1, 2, 3];
-console.log(genCombination(testArray));
+let instance = new MyClass('Nazarii', 'nazar.kishman@gmail.com');
+instance.oldMethod();
